26425cf05a9d2b67c83932cbb31bdf18405da7ad
Joshua Alwinking <65503372+JoshuaisaKing@users.noreply.github.com>
2023-06-17 16:56:34 +0530

    Add files via upload

---
+++ projects/grid-jms/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of editable-grid-jms
+ */
+
+export * from './lib/editable-grid-jms.service';
+export * from './lib/editable-grid-jms.component';
+export * from './lib/editable-grid-jms.module';
