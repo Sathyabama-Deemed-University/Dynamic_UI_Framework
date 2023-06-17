5bca0875f6e56ad78386bfff80ddc0a366af4511
Joshua Alwinking <65503372+JoshuaisaKing@users.noreply.github.com>
2023-06-17 16:54:23 +0530

    Delete projects directory

--- projects/advanced-search/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of advanced-search
- */
-
-export * from './lib/advanced-search.service';
-export * from './lib/advanced-search.component';
-export * from './lib/advanced-search.module';
--- projects/dynamic-jms/ssearch.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { SimpleSearchPipe } from "./ssearch.pipe";
-
-@NgModule({
-    declarations: [SimpleSearchPipe],
-    exports: [SimpleSearchPipe], 
-})
-
-export class SimplesearchModule{}
